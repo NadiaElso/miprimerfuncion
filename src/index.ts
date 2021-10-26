38f6f54382e93d6e36f81a441af465d3a561c2cd
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-10-25 21:20:35 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+function area(base: number = 1, altura: number = 2) {
+  console.log((base * altura) / 2);
+}
+
+area(2, 4);
+console.log(area);
+
+area(3, 6);
+console.log(area);
+
+area(4, 8);
+console.log(area);
+area(5, 10);
+console.log(area);
+
+area(6, 12);
+console.log(area);
+area(7, 14);
+console.log(area);
